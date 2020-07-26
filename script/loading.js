ebb3d889810875fd99841d990f9985e9bc41c512
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-26 14:36:05 +0300

    Add sliding animations for left-bar

--- script/loading.js
+++ script/loading.js
@@ -1,7 +1,19 @@
 const loading = document.getElementsByClassName('loading')[0];
+const leftBar = document.getElementsByClassName('left-bar')[0];
+
+function addAnimations() {
+  let timeOut = 0;
+  [...leftBar.children].forEach( (element) => {
+    setTimeout(() => {
+      element.className += ' swing-from-left'
+    }, timeOut)
+    timeOut += 200;
+  })
+};
 
 window.onload = () => {
   console.log('yüklendik miiiiiiiiiiiiiiiii')
+  addAnimations()
   loading.className = 'loading fade-out';
   setTimeout(() => {
     loading.style.display = 'none'
