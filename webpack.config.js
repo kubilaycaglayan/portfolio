4bdff9ce0e1c264ed4d20dc85112a23227b0cec3
kubilaycaglayan <kblycaglayan@gmail.com>
2020-07-29 22:58:34 +0300

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
 };
