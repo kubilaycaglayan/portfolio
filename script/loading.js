f7338a127f9d671c1876f3584223daea522ae6e2
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-07 13:49:16 +0300

    Fix non iterable object

--- script/loading.js
+++ script/loading.js
@@ -1,14 +1,25 @@
 const loading = document.getElementsByClassName('loading')[0];
 const leftBar = document.getElementsByClassName('left-bar')[0];
+const leftChildren = [];
+for (let i = 0; i < leftBar.children.length; i += 1) {
+  leftChildren.push(leftBar.children[i])
+}
+
+function addClassTimeOut(element, cName, timeOut) {
+  setTimeout(
+    function() {
+      element.className += ' ' + cName
+    }, timeOut
+  )
+}
 
 function addAnimations() {
   let timeOut = 0;
-  [...leftBar.children].forEach( function(element) {
-    setTimeout(function() {
-      element.className += ' swing-from-left'
-    }, timeOut)
+
+  for (let i = 0; i < leftChildren.length; i += 1) {
+    addClassTimeOut(leftChildren[i], 'swing-from-left', timeOut)
     timeOut += 200;
-  })
+  }
 };
 
 function afterWindowLoad() {
